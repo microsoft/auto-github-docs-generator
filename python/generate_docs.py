3b7747fa7acdd3d4efd591527a6963d7085f58f2
Ilya Matiach <ilmat@microsoft.com>
2023-07-21 11:52:34 -0400

    update requests to use new endpoint with question and answer as separate parameters

--- python/generate_docs.py
+++ python/generate_docs.py
@@ -49,8 +49,20 @@ def call_webservice(data, api_key, url):
         print(error.read().decode("utf8", 'ignore'))
     return result
 
-def generate_file(file_name, request, directory):
-    contents = call_webservice({"question": request}, args.key, args.url)
+def print_request_info(question, search):
+    print(f"Question: {question}")
+    print("\n")
+    print(f"Search: {search}")
+    print("\n\n")
+
+def print_response(response):
+    print(f"Response: {response}")
+    print("\n\n")
+
+def generate_file(file_name, search, question, directory):
+    print_request_info(question, search)
+    contents = call_webservice({"question": question, "search": search}, args.key, args.url)
+    print_response(contents)
 
     with open(os.path.join(directory, file_name), "w") as f:
         f.write(contents["output"])
@@ -64,15 +76,18 @@ with open(os.path.join(".", "topics_files_request.txt"), "r") as f:
     topics_files_request = f.read()
     topics_files_request = topics_files_request.replace("{repository_name}", args.repository_name)
 
-print("Topics files request:")
-print(topics_files_request)
-print("\n\n")
+topics_search = "main top-level topics in ml-wrappers repository"
 
-topics = call_webservice({"question": topics_files_request}, args.key, args.url)
+print_request_info(topics_files_request, topics_search)
+
+topics = call_webservice({"question": topics_files_request, "search": topics_search}, args.key, args.url)
+
+print_response(topics)
 
 recommended_topics = topics["output"].split("\n")
 
-rst_file_names_request = f"Generate the rst file names for the {args.repository_name} repository from the topics below.  Do not add numbers to the topics. " + "\n".join(recommended_topics)
+rst_file_names_question = f"Generate the rst file names for the {args.repository_name} repository from the topics below.  Do not add numbers to the topics. \n\n" + "\n".join(recommended_topics)
+rst_file_names_search = topics_search
 
 sample_rst_files = ["contributing.rst",
                     "explainers.rst",
@@ -84,38 +99,60 @@ sample_rst_files = ["contributing.rst",
                     "usage.rst",
                     "visualizations.rst"]
 
-rst_file_names_request += "\nExample:" + "\n".join(sample_rst_files)
+rst_file_names_question += "\n\nExample:" + "\n".join(sample_rst_files)
 
-rst_files = call_webservice({"question": rst_file_names_request}, args.key, args.url)
+print_request_info(rst_file_names_question, rst_file_names_search)
+
+rst_files = call_webservice({"question": rst_file_names_question, "search": rst_file_names_search}, args.key, args.url)
 
 recommended_rst_files = rst_files["output"]
 
-print("Recommended rst files:")
-print(rst_files)
-print("\n\n")
+print_response(topics)
 
 list_rst_files = recommended_rst_files.split("\n")
 
 # Ask the openai endpoint to generate an rst file for each topic
 for rst_file, topic in zip(list_rst_files, recommended_topics):
-    rst_gen_request = f"Generate the {rst_file} file for the {args.repository_name} repository for the topic {topic}. Do not add numbers to the topic. Output as a raw rst file."
-    rst_file_contents = generate_file(rst_file, rst_gen_request, args.directory)
-
-    print(f"Rst file contents for file {rst_file} and topic {topic}:")
-    print(rst_file_contents)
-    print("\n\n")
-
+    rst_topic_search = topic
+    rst_gen_request = f"Generate the {rst_file} file for the {args.repository_name} repository for the topic {topic}. Do not add numbers to the topic. Output as a raw rst file. Do not add any full sentences at the end describing the file. Do not add triple backticks to indicate the file type at the top."
+    rst_file_contents = generate_file(rst_file, rst_topic_search, rst_gen_request, args.directory)
 
 # Ask the openai endpoint to generate the conf.py, index.rst, and .readthedocs.yaml files
 requests = {
     "conf.py": f"Generate the conf.py file for the {args.repository_name} repository.  This should be similar to the file https://github.com/interpretml/interpret-community/blob/main/python/docs/conf.py.",
     "index.rst": f"Generate the index.rst file for the {args.repository_name} repository.  Produce the output as if it were an rst file (do not use triple backticks to indicate a codeblock).  It should reference the previous generated rst files: " + "\n".join(list_rst_files),
-    ".readthedocs.yaml": f"Generate the .readthedocs.yaml file for the {args.repository_name} repository.  Do not include any full sentences at the end describing the yaml."
+    # ".readthedocs.yaml": f"Generate the .readthedocs.yaml file for the {args.repository_name} repository.  Do not include any full sentences at the end describing the yaml."
 }
 
-for file_name, value in requests.items():
+for file_name, question in requests.items():
     directory = args.directory
-    if file_name == ".readthedocs.yaml":
-        directory = args.rootdirectory
-    contents = generate_file(file_name, value, directory)
-    print(f"{file_name} contents:\n{contents}\n\n")
+    # if file_name == ".readthedocs.yaml":
+    #     directory = args.rootdirectory
+    search = topics_search
+    contents = generate_file(file_name, search, question, directory)
+
+# write the .readthedocs.yaml file directly to the root directory
+read_the_docs_content = f"""
+version: 2
+
+build:
+  os: ubuntu-20.04
+  tools:
+    python: "3.8"
+
+sphinx:
+   builder: html
+   configuration: python/docs/conf.py
+
+python:
+   install:
+   - requirements: requirements-doc.txt
+   - method: pip
+     path: python
+
+formats:
+  - epub
+  - pdf
+"""
+with open(os.path.join(args.rootdirectory, ".readthedocs.yaml"), "w") as f:
+    f.write(read_the_docs_content)
