482327e345bb5b77dfe71d0e405aaf333f93cee4
Ilya Matiach <ilmat@microsoft.com>
2023-07-21 11:09:34 -0400

    add back rootdirectory (#6)

--- python/generate_docs.py
+++ python/generate_docs.py
@@ -9,7 +9,7 @@ import ssl
 # Parse input arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--directory', type=str, help='The directory path to generate the docs')
-# parser.add_argument('--rootdirectory', type=str, help='The root directory path to the repository')
+parser.add_argument('--rootdirectory', type=str, help='The root directory path to the repository')
 parser.add_argument('--key', type=str, help='The webservice key')
 parser.add_argument('--url', type=str, help='The webservice url')
 parser.add_argument('--repository-name', type=str, help='The repository name')
@@ -49,7 +49,13 @@ def call_webservice(data, api_key, url):
         print(error.read().decode("utf8", 'ignore'))
     return result
 
+def generate_file(file_name, request, directory):
+    contents = call_webservice({"question": request}, args.key, args.url)
 
+    with open(os.path.join(directory, file_name), "w") as f:
+        f.write(contents["output"])
+
+    return contents
 
 # First, ask the openai endpoint to generate rst files
 
@@ -90,20 +96,10 @@ print("\n\n")
 
 list_rst_files = recommended_rst_files.split("\n")
 
-
-
-def generate_file(file_name, request):
-    contents = call_webservice({"question": request}, args.key, args.url)
-
-    with open(os.path.join(args.directory, file_name), "w") as f:
-        f.write(contents["output"])
-
-    return contents
-
 # Ask the openai endpoint to generate an rst file for each topic
 for rst_file, topic in zip(list_rst_files, recommended_topics):
     rst_gen_request = f"Generate the {rst_file} file for the {args.repository_name} repository for the topic {topic}. Do not add numbers to the topic. Output as a raw rst file."
-    rst_file_contents = generate_file(rst_file, rst_gen_request)
+    rst_file_contents = generate_file(rst_file, rst_gen_request, args.directory)
 
     print(f"Rst file contents for file {rst_file} and topic {topic}:")
     print(rst_file_contents)
@@ -118,5 +114,8 @@ requests = {
 }
 
 for file_name, value in requests.items():
-    contents = generate_file(file_name, value)
+    directory = args.directory
+    if file_name == ".readthedocs.yaml":
+        directory = args.rootdirectory
+    contents = generate_file(file_name, value, directory)
     print(f"{file_name} contents:\n{contents}\n\n")
